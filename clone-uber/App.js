ff19aac56809d41073355d171a3e3dcb994317ba
Joacy <joacymsilva@gmail.com>
2020-07-14 23:05:33 -0300

    Configurando 'react-native-maps'

--- clone-uber/App.js
+++ clone-uber/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function App() {
+export default function App () {
+  const [initialPosition, setInicialPosition] = useState([-12.3288524, -38.7661083]);
+
+  async function loadPosition () {
+    const { status } = await Location.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert('Ooops...', 'Precisamos de sua permissão para obter a localização');
+    }
+
+    const location = await Location.getCurrentPositionAsync();
+
+    const { latitude, longitude } = location.coords;
+
+    setInicialPosition([latitude, longitude]);
+  }
+
+  useEffect(() => {
+    loadPosition();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: initialPosition[0],
+          longitude: initialPosition[1],
+          latitudeDelta: 0.014,
+          longitudeDelta: 0.014,
+        }}
+      />
     </View>
   );
 }
@@ -17,5 +45,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
